9969fd13efacdd678bcaa2f157bcc054c9ea1046
gaijin <ilyaf1999@gmail.com>
2025-10-08 17:40:43 +0300

    create projct

---
+++ api.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from todo import todo_router
+
+app = FastAPI()
+
+todo_list = []
+
+app.get("/")
+async def welcome() -> dict:
+    return { 
+        "message" : "Hello World"
+        }
+
+
+
+app.include_router(todo_router)
