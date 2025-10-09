442d87b1d0322959c3c78a650fcddcb1b7b4bc8c
gaijin <ilyaf1999@gmail.com>
2025-10-09 17:08:32 +0300

    add delete, update operations

--- model.py
+++ model.py
@@ -4,3 +4,21 @@ class Todo(BaseModel):
     id: int
     item: str
 
+    class Config:
+        Schema_extra = {
+            "Example" : {
+                "id" : 1,
+                "item": "Example schema!"
+            }
+        }
+
+
+class TodoItem (BaseModel):
+    item:str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
